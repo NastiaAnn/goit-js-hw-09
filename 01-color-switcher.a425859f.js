84a7d273ada5fa0ad705964558f9efee41f3dc53
NastiaAnn <115037225+NastiaAnn@users.noreply.github.com>
2023-03-27 13:19:20 +0000

    Deploying to gh-pages from @ NastiaAnn/goit-js-hw-09@d2e98b561088310095a83823285d0313959406af 🚀

---
+++ 01-color-switcher.a425859f.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),r=document.body;e.setAttribute("disabled","");let d=null;t.addEventListener("click",(t=>{t.currentTarget.setAttribute("disabled",""),e.removeAttribute("disabled"),d=setInterval((()=>{r.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}),1e3)})),e.addEventListener("click",(()=>{e.setAttribute("disabled",""),clearInterval(d),t.removeAttribute("disabled")}));
+//# sourceMappingURL=01-color-switcher.a425859f.js.map
